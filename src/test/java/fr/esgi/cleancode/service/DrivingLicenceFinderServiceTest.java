9a15a07796d92691189ae8aa128802e8fdc59396
Audrey <adivito@teamdoc.fr>
2022-11-30 10:41:45 +0100

    [NEW Test] should find a driving licence

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,15 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -13,12 +17,15 @@ class DrivingLicenceFinderServiceTest {
     @InjectMocks
     private DrivingLicenceFinderService service;
 
+    private DrivingLicence drivingLicence;
+
     @Mock
     private InMemoryDatabase database;
 
     @Test
     void should_find() {
-
+        database.save(new UUID(4567, 4567), drivingLicence);
+        Assertions.assertNotNull(service.findById(new UUID(4567, 4567)));
     }
 
     @Test
