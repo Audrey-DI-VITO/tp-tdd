f3d6abcaab9c5482048b88fbc85920ec17c64928
Audrey <adivito@teamdoc.fr>
2022-11-30 12:34:38 +0100

    [REFACTO Test] should not find and should find a driving licence

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -2,7 +2,6 @@ package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
 import fr.esgi.cleancode.model.DrivingLicence;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +11,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
 
@@ -19,25 +21,32 @@ class DrivingLicenceFinderServiceTest {
     private DrivingLicenceFinderService service;
 
     @Mock
-    private DrivingLicence drivingLicence;
-
-    @Mock
-    private DrivingLicenceIdGenerationService drivingLicenceIdGenerationService = new DrivingLicenceIdGenerationService();
-
-    @Mock
     private InMemoryDatabase database;
-
-    @Mock
-    UUID uuid = drivingLicenceIdGenerationService.generateNewDrivingLicenceId();
 
     @Test
     void should_find() {
-        database.save(uuid, drivingLicence);
-        Assertions.assertNotNull(service.findById(uuid));
+        final var uuid = UUID.randomUUID();
+        final var drivingLicence = DrivingLicence.builder().id(uuid).build();
+
+        when(database.findById(uuid)).thenReturn(Optional.of(drivingLicence));
+
+        final var actual = service.findById(uuid);
+
+        assertThat(actual).containsSame(drivingLicence);
+        verify(database).findById(uuid);
+        verifyNoMoreInteractions(database);
     }
 
     @Test
     void should_not_find() {
-        Assertions.assertEquals(Optional.empty(), service.findById(drivingLicenceIdGenerationService.generateNewDrivingLicenceId()));
+        final var uuid = UUID.randomUUID();
+
+        when(database.findById(uuid)).thenReturn(Optional.empty());
+
+        final var actual = service.findById(uuid);
+
+        assertThat(actual).isEmpty();
+        verify(database).findById(uuid);
+        verifyNoMoreInteractions(database);
     }
 }
