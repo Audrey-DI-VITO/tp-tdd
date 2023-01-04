390a621c37e3f6af030674796f42059128a70267
Audrey <audrey.divito@protonmail.com>
2023-01-04 12:34:20 +0100

    [Service] LicenceGeneration

---
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceGenerationService.java
@@ -0,0 +1,21 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.model.DrivingLicence;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class DrivingLicenceGenerationService {
+    public DrivingLicence CreateDrivingLicense(String SocialNumber) {
+        return DrivingLicence.builder().id(UUID.randomUUID()).driverSocialSecurityNumber(SocialNumber).build();
+    }
+
+    public void generateLicense(String social_number) {
+    }
+
+    public boolean check_social_number(String social_number) {
+        return social_number.length() == 15 && social_number.matches("[0-9]+");
+    }
+    
+}
