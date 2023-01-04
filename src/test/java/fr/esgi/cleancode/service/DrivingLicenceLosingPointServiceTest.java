f00e1a44e4a31205e1963adc2a4c7afb03074d0b
Audrey <audrey.divito@protonmail.com>
2023-01-04 10:50:35 +0100

    [Test] DrivingLicenceLoosingPoints

---
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceLosingPointServiceTest.java
@@ -0,0 +1,60 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Assertions;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DrivingLicenceLosingPointServiceTest {
+
+    @InjectMocks
+    private DrivingLicenceLosingPointService serviceLosing;
+
+    final String SocialNumber="12345";
+
+    @Mock
+    private InMemoryDatabase database;
+
+    @Test
+    void should_lose_five_points(){
+        DrivingLicence licence = DrivingLicence.builder().id(UUID.randomUUID()).driverSocialSecurityNumber(SocialNumber).build();
+        when(database.findById(licence.getId())).thenReturn(Optional.of(licence));
+        licence = serviceLosing.losing_points(licence.getId(), 5, database);
+        Assertions.assertEquals(7, licence.getAvailablePoints());
+    }
+
+    @Test
+    void should_points_always_be_positive() {
+        DrivingLicence licence = DrivingLicence.builder().id(UUID.randomUUID()).driverSocialSecurityNumber(SocialNumber).build();
+        when(database.findById(licence.getId())).thenReturn(Optional.of(licence));
+        licence = serviceLosing.losing_points(licence.getId(), 15, database);
+        Assertions.assertEquals(0, licence.getAvailablePoints());
+    }
+
+    @Test
+    void should_be_updated_in_database() {
+        DrivingLicence licence = DrivingLicence.builder().id(UUID.randomUUID()).driverSocialSecurityNumber(SocialNumber).build();
+        when(database.findById(licence.getId())).thenReturn(Optional.of(licence));
+        licence = serviceLosing.losing_points(licence.getId(), 5, database);
+        when(database.findById(licence.getId())).thenReturn(Optional.of(licence));
+
+        final var actual = database.findById(licence.getId());
+        var number = 0;
+
+        if(actual.isPresent()) {
+            number = actual.get().getAvailablePoints();
+        }
+        assertThat(number).isEqualTo(7);
+    }
+}
